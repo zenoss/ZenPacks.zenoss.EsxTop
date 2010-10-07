b6dbfcf54f209b4404daaa5fc3146ca199785668
kkearney <kkearney@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-10-07 19:55:04 +0000

    Ignore pools. Reviewed by Chip
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.EsxTop@36232 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.EsxTop"
-VERSION = "0.1.1"
+VERSION = "1.0.0"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
